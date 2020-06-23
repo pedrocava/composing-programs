cbda158048ed32688577c32fe1845b14af2235a0
Pedro Cavalcante <pedrocolrj@gmail.com>
2020-06-23 15:59:23 -0300

    comecei o cap2

---
+++ cap2.py
@@ -0,0 +1,27 @@
+42
+max(7, 9)
+
+pow(100, 2)
+pow(2, 100)
+
+from math import sqrt
+
+sqrt(2)
+
+import math 
+
+22*pi/3
+
+def mul(a, b):
+    return a*b
+
+def square(x):
+    return mul(x, x)
+
+square(2)
+
+def sumOfSquares(a, b):
+    return sum(square(a), square(b))
+
+
+    
